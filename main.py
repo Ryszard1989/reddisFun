bb334805ae530c3e46e5ed6d7bc62697100513ad
Ryszard1989 <rys.szulc@gmail.com>
2021-04-17 16:06:24 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#TODO:
+#Unit tests.
+#retrieve top score
+#Implement reddis for storage
+#retrieve score from reddis
+#Sonarcube coverage
+#Docker
+#Try beat reddis with different message sizes
+#Play with probabilistic versus regular query.
+
+import redis
+
+def main():
+    r = redis.Redis()
+    scores = makeSomeData()
+    r.mset(scores)
+    print(r.get("Bahamas"))
+    print(r.get("Claude"))
+
+    #data = makeSomeData()
+    #store data in reddis
+    #get data from reddis
+    #for d,s in data.items():
+    #    print (str(d) + " scored " + str(s))
+
+def makeSomeData():
+    scores = {"Rysz":9000, "Claude":4200, "Abe":30,"Bob":1400,"Charlie":1900}
+    return scores
+
+
+if __name__ == "__main__":
+    main()
